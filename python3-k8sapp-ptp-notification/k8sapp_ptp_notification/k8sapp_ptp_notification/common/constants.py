178914b535d071d7a6dad8467b44ff5b7a2c6266
Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>
2023-04-01 03:01:42 -0400

    Remove psp-rolebinding from ptp-notification-armada-app
    Removed the psp-rolebinding helm chart from being
    packaged as part of ptp-notification-armada-app.
    
    PASS: Build pkgs
    PASS: system-application apply ptp-notification
    PASS: application pkg doesn't consist psp-rolebinding
          helm chart
    
    Story: 2010590
    Task: 47769
    
    Signed-off-by: Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>
    Change-Id: I15fb26a11d2bba4c51e0144e4c4435e7f27f9e52

--- python3-k8sapp-ptp-notification/k8sapp_ptp_notification/k8sapp_ptp_notification/common/constants.py
+++ python3-k8sapp-ptp-notification/k8sapp_ptp_notification/k8sapp_ptp_notification/common/constants.py
@@ -7,4 +7,3 @@
 # Helm: Supported charts:
 # These values match the names in the chart package's Chart.yaml
 HELM_CHART_PTP_NOTIFICATION = 'ptp-notification'
-HELM_CHART_PSP_ROLEBINDING = 'ptp-notification-psp-rolebinding'
