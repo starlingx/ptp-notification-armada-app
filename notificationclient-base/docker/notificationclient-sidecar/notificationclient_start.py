aec7acc2d7abc581f24692927299cbf65d6bfba8
Andre Mauricio Zelak <andre.zelak@windriver.com>
2026-03-23 18:53:47 -0300

    Replace built-in WSGI SimpleServer with Waitress
    To improve stability and performance in live environments, the default
    WSGI SimpleServer has been replaced. A new sidecar container image
    has been created that runs the application under Waitress instead.
    
    Why switch to Waitress for the PTP notification client?
    * Production-ready and certified by REST security scanners.
    * Handles multiple simultaneous client connections; currently
      configured to allow four concurrent connections.
    
    Test Plan:
    PASS Build notificationclient-base container
          docker build --build-arg \
           BASE="starlingx/stx-debian:master-stable-latest" \
           -f ./Dockerfile ../../docker \
           -t notificationclient-base:waitress
    PASS Applied and removed ptp-demo chart using the new
        container image.
    PASS Basic operation get, subscribe, list and delete
        subscription.
    PASS Attack using a malicious GET request.
    PASS OWASP ZAP active scanner tool
          sudo docker run -t -v /tmp:/zap/wrk:rw --network="host" \
           ghcr.io/zaproxy/zaproxy:stable zap-full-scan.py \
           -t http://172.16.192.65:8080 -r report.html
    PASS Wapiti scanner tool
          sudo docker run --rm -it --network="host" \
           -v /tmp:/app/output cyberwatch/wapiti \
           -u "http://172.16.192.65:8080/" \
           -o /app/output/ --format html
    PASS Nikto scanner tool
          sudo docker run --rm -v /tmp:/output --network="host" \
           alpine/nikto:latest -h http://172.16.192.65:8080/ \
           -o /output/nikto-results.txt
    PASS CVE scanner returned no critical issues.
    
    Partial-Bug: 2145780
    
    Change-Id: I5875244d8a43877090e43d56fb61b6b5cb5abec3
    Signed-off-by: Andre Mauricio Zelak <andre.zelak@windriver.com>

--- notificationclient-base/docker/notificationclient-sidecar/notificationclient_start.py
+++ notificationclient-base/docker/notificationclient-sidecar/notificationclient_start.py
@@ -1,17 +1,14 @@
 #
-# Copyright (c) 2024 Wind River Systems, Inc.
+# Copyright (c) 2024, 2026 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 import logging
-import os
-import socket
-from wsgiref import simple_server
-
-from netaddr import IPAddress
 from notificationclientsdk.common.helpers import log_helper
+import os
 from pecan.deploy import deploy
+from waitress import serve
 
 LOG = logging.getLogger(__name__)
 log_helper.config_logger(LOG)
@@ -22,27 +19,10 @@ SIDECAR_API_PORT = int(
     os.environ.get("SIDECAR_API_PORT", '8080'))
 
 
-def get_address_family(ip_string):
-    """
-    Get the family for the given ip address string.
-    """
-    ip_address = IPAddress(ip_string)
-    if ip_address.version == 6:
-        return socket.AF_INET6
-    else:
-        return socket.AF_INET
-
-
 def main():
-    simple_server.WSGIServer.address_family = get_address_family(
-        SIDECAR_API_HOST)
     application = deploy('/opt/notificationclient/config.py')
-
-    with simple_server.make_server(SIDECAR_API_HOST,
-                                   SIDECAR_API_PORT,
-                                   application) as httpd:
-        LOG.info("notificationclient_start.py: Starting notificationclient")
-        httpd.serve_forever()
+    LOG.info("notificationclient_start.py: Starting notificationclient")
+    serve(application, host=SIDECAR_API_HOST, port=SIDECAR_API_PORT)
 
 
 if __name__ == "__main__":
